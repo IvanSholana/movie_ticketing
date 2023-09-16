34a1706cf73ad94068e18b63bfc7f3a4e0248e24
Ivan Sholana <ivansholana@gmail.com>
2023-09-16 22:47:07 +0700

    add login page

--- lib/screens/login/main-logis-screen.dart
+++ lib/screens/login/main-logis-screen.dart
@@ -1,93 +1,86 @@
-  import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-  class LoginScreen extends StatefulWidget {
-    @override
-    State<LoginScreen> createState() => _LoginScreenState();
-  }
-
-  class _LoginScreenState extends State<LoginScreen> {
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        body: Container(
-          width: double.infinity,
-          height: double.infinity,
-          child: Stack(
-            children: [
-              Positioned.fill(
-                child: Image.asset(
-                  "assets/images/dolittle.jpg",
-                  fit: BoxFit.cover, // Memastikan gambar sesuai layar
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          // Gambar latar belakang
+          Image.asset(
+            "assets/images/dolittle.jpg",
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.cover, // Memastikan gambar sesuai layar
+          ),
+          Positioned.fill(
+            child: Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.black.withOpacity(0.7), Colors.transparent],
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
                 ),
               ),
-              Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Container(
-                  height: double.infinity,
-                  width: double.infinity,
-                  padding: const EdgeInsets.only(left: 20, right: 20, bottom: 50),
-                  decoration: const BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [Colors.black, Colors.transparent],
-                      begin: Alignment.bottomCenter,
-                      end: Alignment.topCenter,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Image.asset("assets/images/dolittle-text.png"),
+                  const Text(
+                    "Buy and Watch Your Favorite Movie with Us",
+                    style: TextStyle(color: Colors.white, fontSize: 17),
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      // Tambahkan logika ketika tombol LOGIN ditekan
+                    },
+                    style: TextButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      backgroundColor: Colors.red,
+                      padding: const EdgeInsets.symmetric(vertical: 12),
+                      minimumSize: const Size(double.infinity, 50),
+                    ),
+                    child: const Text(
+                      "LOGIN",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
                     ),
                   ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      TextButton(
-                        onPressed: () {},
-                        style: TextButton.styleFrom(
-                          shape: const RoundedRectangleBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(10))),
-                          backgroundColor: Colors.blueGrey,
-                          padding: const EdgeInsets.symmetric(
-                            vertical: 12, // Tambahkan padding vertical
-                          ),
-                          minimumSize:
-                              const Size(double.infinity, 50), // Lebar penuh
-                        ),
-                        child: const Text(
-                          "LOGIN",
-                          style: TextStyle(
-                            color: Colors.white, // Ubah warna teks
-                            fontSize: 18, // Sesuaikan ukuran font
-                          ),
-                        ),
+                  // Tombol SIGN IN
+                  TextButton(
+                    onPressed: () {
+                      // Tambahkan logika ketika tombol SIGN IN ditekan
+                    },
+                    style: TextButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
                       ),
-                      const SizedBox(height: 16), // Jarak antara tombol
-                      TextButton(
-                        onPressed: () {},
-                        style: TextButton.styleFrom(
-                          shape: const RoundedRectangleBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(10))),
-                          backgroundColor: Colors.blue[100],
-                          padding: const EdgeInsets.symmetric(
-                            vertical: 12, // Tambahkan padding vertical
-                          ),
-                          minimumSize:
-                              const Size(double.infinity, 50), // Lebar penuh
-                        ),
-                        child: const Text(
-                          "SIGN IN",
-                          style: TextStyle(
-                            color: Colors.white, // Ubah warna teks
-                            fontSize: 18, // Sesuaikan ukuran font
-                          ),
-                        ),
+                      padding: const EdgeInsets.symmetric(vertical: 12),
+                      minimumSize: const Size(double.infinity, 50),
+                    ),
+                    child: const Text(
+                      "DONT HAVE ACCOUNT? SIGN IN",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
                       ),
-                    ],
+                    ),
                   ),
-                ),
-              )
-            ],
+                ],
+              ),
+            ),
           ),
-        ),
-      );
-    }
+        ],
+      ),
+    );
   }
+}
